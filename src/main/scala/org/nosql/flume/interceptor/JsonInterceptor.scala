243528d8a5594214666fd187062fc9097efc5cbb
Juan Bono <juanbono94@gmail.com>
2018-06-26 07:14:47 -0300

    convert to lowercase the tweet before processing it

--- src/main/scala/org/nosql/flume/interceptor/JsonInterceptor.scala
+++ src/main/scala/org/nosql/flume/interceptor/JsonInterceptor.scala
@@ -3,7 +3,6 @@ package org.nosql.flume.interceptor
 import java.io.{BufferedWriter, File, FileWriter}
 import java.nio.charset.StandardCharsets
 
-import ujson._
 import org.apache.flume.interceptor.Interceptor
 import org.apache.flume.{Context, Event}
 
@@ -15,7 +14,7 @@ class JsonInterceptor(ctx: Context) extends Interceptor {
   private var bw: BufferedWriter = _
 
   def initialize(): Unit = {
-    file = new File("./log_scala.txt")
+    file = new File("./tweet_log.txt")
     bw = new BufferedWriter(new FileWriter(file))
   }
 
@@ -33,12 +32,11 @@ class JsonInterceptor(ctx: Context) extends Interceptor {
 
     val wordToFilter = ctx.getString("json_filter.word")
 
-    if (text.contains(wordToFilter))
+    if (text.toLowerCase().contains(wordToFilter))
       bw.append(text)
 
     event
   }
-
 
   // handles a list of tweets
   def intercept(list: java.util.List[Event]): java.util.List[Event] = {
